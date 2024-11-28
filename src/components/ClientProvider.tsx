728a31d7b7252d390f16d261eabb22faeefb2d5b
Ayan <nazbievayan16@gmail.com>
2024-11-28 17:50:26 +0500

    env key

--- src/components/ClientProvider.tsx
+++ src/components/ClientProvider.tsx
@@ -1,10 +1,15 @@
 // src/components/ClientProvider.tsx
 'use client';
 
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import {store} from "@/src/store/store";
+import {ReactNode} from "react";
 
-const ClientProvider = ({ children }) => {
+interface ClientProviderProps {
+    children: ReactNode;
+}
+
+const ClientProvider = ({children}:ClientProviderProps) => {
     return <Provider store={store}>{children}</Provider>;
 };
 
--- src/features/weatherAPI.ts
+++ src/features/weatherAPI.ts
@@ -1,7 +1,7 @@
 // Need to use the React-specific entry point to import createApi
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const API_KEY = '6beda4838f1980561fc6fbaba7a10f3c'
+const API_KEY = process.env.REACT_APP_API_KEY
 
 // Define a service using a base URL and expected endpoints
 export const weatherApi = createApi({
