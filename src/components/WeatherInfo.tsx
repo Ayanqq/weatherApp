93e797593827fecd2c70ac83d1f74eb8039377be
Ayan <nazbievayan16@gmail.com>
2024-11-29 16:23:29 +0500

    kelvin to cels upd

--- src/components/WeatherInfo.tsx
+++ src/components/WeatherInfo.tsx
@@ -6,15 +6,26 @@ interface WeatherInfoProps {
 
 const WeatherInfo = ({city}: WeatherInfoProps) => {
     const {data, error, isLoading} = useGetWeatherQuery(city);
+    const kelvin = 273.15
+    // const currentWeather = Number()
+
+    let tempToCelsius
+
+    // console.log(data.main.temp)
 
     if (isLoading) return <p>Загрузка...</p>;
     if (error) return <p>Ошибка: {(error as Error).message}</p>;
-    if (!data) return null;
+    if (!data) {
+        return null
+    } else {
+        tempToCelsius = (Number(data.main.temp) - kelvin).toFixed(1);
+    }
+
 
     return (
         <div>
             <h2>Погода в {data.name}</h2>
-            <p>Температура: {data.main.temp}°C</p>
+            <p>Температура: {tempToCelsius}°C</p>
             <p>Влажность: {data.main.humidity}%</p>
             <p>Скорость ветра: {data.wind.speed} м/с</p>
         </div>
--- src/features/weatherAPI.ts
+++ src/features/weatherAPI.ts
@@ -1,14 +1,26 @@
 // Need to use the React-specific entry point to import createApi
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const API_KEY = process.env.REACT_APP_API_KEY
+const API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY
+console.log('API-KEY:', API_KEY)
+
+type WeatherType = {
+    name:string;
+    main: {
+        temp:string;
+        humidity:string
+    }
+    wind: {
+        speed:string;
+    }
+}
 
 // Define a service using a base URL and expected endpoints
 export const weatherApi = createApi({
     reducerPath: 'weatherAPI',
     baseQuery: fetchBaseQuery({ baseUrl: 'https://api.openweathermap.org/data/2.5/' }),
     endpoints: (builder) => ({
-        getWeather: builder.query<any, string>({
+        getWeather: builder.query<WeatherType, string>({
             query: (city) => `weather?q=${city}&appid=${API_KEY}`,
         }),
     }),
