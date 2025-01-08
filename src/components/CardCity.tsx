ce6f261c4ae43992587975ac3a107344936dc599
Ayan <nazbievayan16@gmail.com>
2025-01-08 13:16:28 +0500

    need to refactor

--- src/components/CardCity.tsx
+++ src/components/CardCity.tsx
@@ -7,35 +7,28 @@ type CardCityType = {
 
 export const CardCity = ({city}: CardCityType) => {
     const {data} = useGetWeatherQuery(city);
-    const localCity = localStorage.getItem('city');
+    localStorage.setItem("city", 'Almaty' || data.name);
     const localTimezoneOffset = new Date().getTimezoneOffset() * 60
+    const localCityName = localStorage.getItem('city')
     let totalTime = null
 
     if (data) {
-        const timestamp = data.dt;  // Unix timestamp
-        const timezoneOffset = data.timezone;  // Смещение для города
+        const timestamp = data.dt;
+        const timezoneOffset = data.timezone;
+        const localTime = new Date((timestamp + timezoneOffset + localTimezoneOffset) * 1000)
 
-        // Если смещение города совпадает с локальным, то не добавляем смещение
-        const isSameTimezone = timezoneOffset === localTimezoneOffset;
-
-        const localTime = isSameTimezone
-            ? new Date(timestamp * 1000)  // Просто без изменений
-            : new Date((timestamp + timezoneOffset) * 1000);  // Применяем смещение
-
-        const formattedTime = localTime.toLocaleString('en-US', {
+        totalTime = localTime.toLocaleString('en-US', {
             hour: '2-digit',
             minute: '2-digit',
             hour12: false,
         });
-
-        totalTime = formattedTime; // Покажет правильное время в зависимости от часового пояса
     }
 
 
     return (
         <Container minWidth={'510px'}>
             <div className={'flex flex-col text-center relative'}>
-                <span className={'text-[36px] font-bold text-[#3F3F3F]'}>{localCity}</span>
+                <span className={'text-[36px] font-bold text-[#3F3F3F]'}>{localCityName}</span>
                 <div className={'w-full gap-[2px]'}>
                     <span className={'text-[96px] font-bold text-[#3F3F3F]'}>{totalTime}</span>
                     <span className={'absolute top-[165px] right-[65px]'}>Thursday, 31 Aug</span>
