1110ee248a57f8cfba8240415ca0a3bf91576b08
Ayan <nazbievayan16@gmail.com>
2025-01-28 18:00:54 +0500

    some refactor

--- src/app/weather/page.tsx
+++ src/app/weather/page.tsx
@@ -35,9 +35,6 @@ const WeatherPage = () => {
                     <HourlyForecast/>
                 </div>
             </div>
-            {/*{*/}
-            {/*    city && <WeatherInfo city={city}/>*/}
-            {/*}*/}
         </div>
     )
 };
